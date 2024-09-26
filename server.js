7f84f13c82884013f44bae51360304f3e2a07d4c
Vangari-Rajesh <118105367+Vangari-Rajesh@users.noreply.github.com>
2024-09-26 07:16:07 +0530

    Update server.js build

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import Razorpay from "razorpay";
 import paymentRoute from "./routes/userRoutes.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import cors from "cors";
 import connectDB from "./db/connect.js";
 import userRouter from "./routes/userRoutes.js";
@@ -12,17 +14,19 @@ import availableWasteReqRouter from "./routes/availableWasteReqRoutes.js";
 import cartRouter from "./routes/cart.js";
 
 dotenv.config();
-
-console.log("MONGO_URI:", process.env.MONGO_URI);
-console.log("RAZORPAY_API_KEY:", process.env.RAZORPAY_API_KEY);
-console.log("RAZORPAY_APT_SECRET:", process.env.RAZORPAY_APT_SECRET);
-console.log("PORT:", process.env.PORT);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' })); // to resolve payload large error
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(userRouter);
 app.use(wasteReqRouter);
@@ -43,6 +47,9 @@ app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // Export the app and instance
 export default app;
 export { instance };
